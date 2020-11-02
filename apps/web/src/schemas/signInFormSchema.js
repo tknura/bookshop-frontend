975d22e80873b341ed77201120e2c95b8a300e3d
Tomasz Knura <tomasz.knura@itsilesia.com>
2020-11-03 00:35:07 +0100

    feat: add validation to sign in form (#1)

---
+++ apps/web/src/schemas/signInFormSchema.js
@@ -0,0 +1,11 @@
+import * as yup from 'yup'
+
+export const signInSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('common.errors.email.format')
+    .required('common.errors.email.required'),
+  password: yup
+    .string()
+    .required('common.errors.password.required'),
+})
--- apps/web/src/screens/SignInUpScreen/SignInForm/SignInForm.js
+++ apps/web/src/screens/SignInUpScreen/SignInForm/SignInForm.js
@@ -1,6 +1,9 @@
-import { Container, Button, TextField } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
+import { Link, Container, Button, TextField } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { useFormik } from 'formik'
+import { useTranslation } from 'react-i18next'
+import { signInSchema } from '../../../schemas/signInFormSchema'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,36 +19,67 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-between',
     marginTop: theme.spacing(2),
     padding: 0,
-  }
+  },
 }))
 
 const SignInForm = () => {
   const classes = useStyles()
+  const [t] = useTranslation()
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    errors,
+    touched
+  } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    // TO DO add api fetching
+    onSubmit: () => null,
+    validationSchema: signInSchema,
+  })
 
   return (
     <form autoComplete="off" className={classes.root}>
       <TextField
-        label="E-mail"
-        variant="filled"
+        id="email"
+        value={values.email}
+        error={touched.email && errors.email}
+        helperText={touched.password && t(errors.email)}
+        onChange={handleChange}
+        required
+        label={t('common.email')}
+        variant="outlined"
         className={classes.textField}
       />
       <TextField
-        label="Password"
-        variant="filled"
+        id="password"
+        values={values.password}
+        error={touched.password && errors.password}
+        helperText={touched.password && t(errors.password)}
+        onChange={handleChange}
+        required
+        type="password"
+        label={t('common.password')}
+        variant="outlined"
         className={classes.textField}
       />
       <Container className={classes.buttonContainer}>
-        <Button
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <Link
+          component="button"
           href="#"
         >
-          Reset Password
-        </Button>
+          {t('screen.signIn.buttons.resetPassword')}
+        </Link>
         <Button
           variant="outlined"
           color="primary"
-          className={classes.button}
+          onClick={handleSubmit}
         >
-          Sing in
+          {t('screen.signIn.buttons.signIn')}
         </Button>
       </Container>
     </form>
