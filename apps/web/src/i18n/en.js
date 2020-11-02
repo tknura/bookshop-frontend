d072ea09159fc4579c44d0431ead9f5ce45e5edd
Tomasz Knura <tomasz.knura@itsilesia.com>
2020-11-03 00:34:31 +0100

    feat: add i18n (#1)

---
+++ apps/web/src/i18n/en.js
@@ -0,0 +1,25 @@
+export const en = {
+  translation: {
+    common: {
+      email: 'E-mail',
+      password: 'Password',
+      errors: {
+        email: {
+          required: 'E-mail adress is required.',
+          format: 'E-mail adress is incorect.',
+        },
+        password: {
+          required: 'Password is required.',
+        },
+      }
+    },
+    screen: {
+      singIn: {
+        buttons: {
+          singIn: 'Sign in',
+          resetPassword: 'Reset password'
+        }
+      }
+    },
+  }
+}
---
+++ apps/web/src/i18n/pl.js
@@ -0,0 +1,25 @@
+export const pl = {
+  translation: {
+    common: {
+      email: 'E-mail',
+      password: 'Hasło',
+      errors: {
+        email: {
+          required: 'Adres e-mail jest wymagany',
+          format: 'Adres e-mail jest niepoprawny.',
+        },
+        password: {
+          required: 'Hasło jest wymagane',
+        },
+      }
+    },
+    screen: {
+      signIn: {
+        buttons: {
+          signIn: 'Zaloguj się',
+          resetPassword: 'Zresetuj hasło'
+        }
+      }
+    },
+  }
+}
